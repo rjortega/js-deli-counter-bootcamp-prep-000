4131c657189ca46f13254b74357488d524e90b94
Ricardo Ortega <rikrdo.ortega@gmail.com>
2019-04-22 18:10:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
+// var i = 0
+// function takeANumber(line){
+//   i++
+//   line.push(i)
+//   return i
+// }
+//
+// takeANumber(line) -> 1
+// takeANumber(line) -> 2
+// noseSeving(line) -> 1
+// nowServing(line) -> 2
+// takeANumber(line) -> 3
+
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length > 0){
+  if (katzDeliLine.length){
     return "Currently serving " + katzDeliLine.shift() + "."
   } else {
     return "There is nobody waiting to be served!"
@@ -21,3 +34,16 @@ function currentLine(katzDeliLine){
   }
   return `The line is currently:${katzDeliLine}`
 }
+/*
+function currentLine(katzDeliLine) {
+  if (!katzDeliLine.length) {
+    return "The line is currently empty."
+  } else {
+  const numbersAndNames = []
+  for (let i = 0, l = katzDeliLine.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${katzDeliLine[i]}`)
+  }
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+  }
+}
+*/
